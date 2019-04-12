72d2f67878ff60e7d85ae593cbd7ce21a4323b18
ESM Smart Solutions <esm.smart.solutions@outlook.com>
2019-04-12 12:06:50 -0400

    Add project files.

---
+++ LocalNotifications_DNStd/LocalNotifications_DNStd/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Plugin.LocalNotifications;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LocalNotifications_DNStd
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        const int _SAMPLE_ID = 1;
+        int _secondsToDelivery;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void ScheduledSwitchToggled(object sender, ToggledEventArgs e)
+        {
+            ScheduleSecondsPicker.IsVisible = ScheduleSwitch.IsToggled;
+
+            if (!ScheduleSwitch.IsToggled)
+            {
+                _secondsToDelivery = 0;
+            }
+        }
+
+        void Handle_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            _secondsToDelivery = int.Parse(ScheduleSecondsPicker.Items[ScheduleSecondsPicker.SelectedIndex]);
+        }
+
+        void SendButtonClicked(object sender, EventArgs e)
+        {
+            if (_secondsToDelivery > 0)
+            {
+                CrossLocalNotifications.Current.Show(TitleEntry.Text, BodyEntry.Text, _SAMPLE_ID, DateTime.Now.AddSeconds(_secondsToDelivery));
+            }
+            else
+            {
+                CrossLocalNotifications.Current.Show(TitleEntry.Text, BodyEntry.Text, _SAMPLE_ID);
+            }
+        }
+
+        private void CancelButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
